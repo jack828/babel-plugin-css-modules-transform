a91255fc342167a61c237391af9f7315d884ed3f
Jack Burgess <jackburgess124@gmail.com>
2018-08-30 11:43:36 +0100

    >Improve logic to empty result file if CSS string is empty

--- src/utils/writeCssFile.js
+++ src/utils/writeCssFile.js
@@ -6,16 +6,17 @@ import { readFileSync, appendFileSync, writeFileSync } from 'fs';
  * Writes css file to given path (and creates directories)
  *
  * Check existance of CSS string, and compare file contents
- * length. If the CSS string is larger, override the file.
+ * length. If the CSS string is larger, overwrite the file.
+ * If the CSS string is empty, then overwrite the file with it (read: delete it)
  *
  * @param {String} path
  * @param {String} content
  */
 export default function writeCssFile(path, content) {
     mkdirp.sync(dirname(path));
-    const contents = readFileSync(path, 'utf8');
+    const fileContents = readFileSync(path, { encoding: 'utf8', flag: 'a+' });
     let append = true;
-    if (contents && content.length > contents.length) {
+    if (content.length === 0 || content.length >= fileContents.length) {
         append = false;
     }
 
